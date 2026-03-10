96194a0648f4eaabc29c1ad992276de920128248
rajee-embroidery <s.rakshithajeevaragini@gmail.com>
2026-03-10 22:46:30 +0530

    Bulk add 1 designs via Admin Portal

--- js/database.js
+++ js/database.js
@@ -6,5 +6,13 @@ window.CATALOG_DATA = [
         "price": "400.00",
         "imageUrl": "images/designs/400-7618951604.jpeg",
         "dateAdded": "2026-03-10T17:15:53.623Z"
+    },
+    {
+        "id": "400-5900980631",
+        "title": "400",
+        "category": "400-rs",
+        "price": "400.00",
+        "imageUrl": "images/designs/400-5900980631.jpeg",
+        "dateAdded": "2026-03-10T17:16:23.304Z"
     }
 ];
