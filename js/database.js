91478b39071839bfc6d486911ff0bd7f98eaa255
rajee-embroidery <s.rakshithajeevaragini@gmail.com>
2026-03-10 22:43:18 +0530

    Deploying js/database.js via API

--- js/database.js
+++ js/database.js
@@ -1,10 +1,1 @@
-window.CATALOG_DATA = [
-    {
-        "id": "400-4706583172",
-        "title": "400",
-        "category": "400-rs",
-        "price": "400.00",
-        "imageUrl": "images/designs/400-4706583172.jpeg",
-        "dateAdded": "2026-03-10T17:09:45.562Z"
-    }
-];
+﻿window.CATALOG_DATA = ;
