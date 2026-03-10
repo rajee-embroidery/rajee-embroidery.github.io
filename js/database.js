d95816ca03c8ba05e2367ddd952f4521ff577a7c
rajee-embroidery <s.rakshithajeevaragini@gmail.com>
2026-03-10 22:50:27 +0530

    Bulk add 1 designs via Admin Portal

--- js/database.js
+++ js/database.js
@@ -1,1 +1,10 @@
-﻿window.CATALOG_DATA = ;
+window.CATALOG_DATA = [
+    {
+        "id": "WhatsApp-Image-2026-03-10-at-9-8317216470",
+        "title": "WhatsApp Image 2026-03-10 at 9 59 12 PM",
+        "category": "400-rs",
+        "price": "400.00",
+        "imageUrl": "images/designs/WhatsApp-Image-2026-03-10-at-9-8317216470.jpeg",
+        "dateAdded": "2026-03-10T17:20:19.852Z"
+    }
+];
